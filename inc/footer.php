716da803b8854c04c0d2583fe9fdaa8806f7d973
Garrett Smith <dhtmlkitchen@gmail.com>
2014-08-01 16:06:11 -0700

    remove unused files

--- inc/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-    <a href="/" rel="home">
-         Thai Massage - San Francisco - Boonsong Somjit
-    </a>
-    <a href="http://www.genbook.com/bookings/slot/reservation/30134154/192164703/192182013/?bookingContactId=209103260"
-        ><img alt="book an appointment" title="Make an Appointment with Song." id="genbook-footer"
-    src="http://www.genbook.com/bookings/images/booknow.gif" class="genbook"
-    ></a>
-</div>
--- inc/heading.php
+++
@@ -1,9 +0,0 @@
-<img src="/img/bs-25.jpg" title="Thai Massage Therapist Boonsong Somjit"
-   alt="Thai Massage Therapist Boonsong Somjit" id="bs">
-<blockquote>
-    <p id="love">
-        <q>I love to massage and I am happy to see people get better after I have done my session.</q>
-    </p>
-</blockquote>
-
-<h1><a href="/">Song <span>Thai</span> Massage</a></h1>
